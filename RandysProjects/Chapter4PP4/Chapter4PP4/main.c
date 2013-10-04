3056cf368195726a0b7378c15f3e41d19be05dd1
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-10-04 17:03:21 -0400

    modified:   RandysProjects/Chapter4PP4/Chapter4PP4/main.c

--- RandysProjects/Chapter4PP4/Chapter4PP4/main.c
+++ RandysProjects/Chapter4PP4/Chapter4PP4/main.c
@@ -8,132 +8,108 @@
 
 #include <stdio.h>
 
-#define TWENTYSEVENHUNDRED 2700
-#define THIRTYFIVEHUNDRED 3500
-#define THIRTYEIGHTHUNDRED 3800
-#define ONE 1
-#define TWO 2
-#define THREE 3
-#define FOUR 4
-#define FIVE 5
-#define SIX 6
-#define SEVEN 7
-#define EIGHT 8
+#define TWENTYSEVENHUNDRED	2700
+#define THIRTYFIVEHUNDRED	3500
+#define THIRTYEIGHTHUNDRED	3800
 
-int year;
-int weight;
-int weightClass;
+// Weight Class
+#define ONE					1
+#define TWO					2
+#define THREE				3
+#define FOUR				4
+#define FIVE				5
+#define SIX					6
+#define SEVEN				7
+#define EIGHT				8
 
-int twentySevenHundred;
-int thirtyEightHundred;
+// Fee Schedule
+#define ONE_FEE				16.50
+#define TWO_FEE				25.50
+#define THREE_FEE			46.50
+#define FOUR_FEE			27.00
+#define FIVE_FEE			30.50
+#define SIX_FEE				52.50
+#define SEVEN_FEE			35.50
+#define EIGHT_FEE			65.50
+
+int		year;
+int		weight;
+int		weightClass;
+float	fee;
+int		twentySevenHundred;
+int		thirtyEightHundred;
 
 void welcomeUser();
+
 void inputYear();
+
 void inputWeight();
 
-float computeFee(int y, int w);
+void computeFee(int y, int w);
+
 void reportFee();
 
-void welcomeUser(){
-
-
-    printf("Welcom e to this super awesome program!\n\n");
-    
-    
-}
-
-int main(int argc, const char * argv[])
+void welcomeUser()
 {
-    welcomeUser();
-
-    inputYear();
-    inputWeight();
-    computeFee(year, weight);
-    
-    return 0;
+	printf("Welcom e to this super awesome program!\n\n");
 }
 
-void inputYear(){
+int main(int argc, const char *argv[])
+{
+	welcomeUser();
 
-    printf("Please enter the Model Year --> ");
-    scanf("%d",&year);
+	inputYear();
+	inputWeight();
+	computeFee(year, weight);
 
-    
+	return 0;
 }
 
-
-void inputWeight(){
-
-   printf("Please enter the Model Weight --> ");
-    scanf("%d",&weight);
-
+void inputYear()
+{
+	printf("Please enter the Model Year --> ");
+	scanf("%d", &year);
 }
 
-float computeFee(int y, int w){
-
-    float fee = 0.0;
-    
-    if (year <= 1970){
-    
-        if (weight < TWENTYSEVENHUNDRED){
-        
-            weightClass = ONE;
-        
-        }
-        
-        else if (weight <= THIRTYEIGHTHUNDRED){
-        
-            weightClass = TWO;
-        
-        }
-        
-        else if (weight > THIRTYEIGHTHUNDRED){
-        
-            
-            weightClass = THREE;
-        
-        }
-        
-    }
-    
-    else if (year <= 1979){
-   
-        if (weight < TWENTYSEVENHUNDRED){
-        
-            
-            weightClass = FOUR;
-        }
-        else if (weight <= THIRTYEIGHTHUNDRED){
-        
-            weightClass = FIVE;
-        
-        }
-        else if (weight > THIRTYEIGHTHUNDRED){
-        
-        
-            weightClass = SIX;
-        }
-    
-    }
-    
-    else if (year >= 1980){
-    
-        if (weight < THIRTYFIVEHUNDRED){
-        
-            weightClass = SEVEN;
-            
-        }
-        
-        else if (weight >= THIRTYFIVEHUNDRED){
-        
-            weightClass = EIGHT;
-            
-        }
-    
-    }
-    
-    
-    return fee;
+void inputWeight()
+{
+	printf("Please enter the Model Weight --> ");
+	scanf("%d", &weight);
 }
 
-void reportFee(){}
+void computeFee(int y, int w)
+{
+	if (year <= 1970) {
+		if (weight < TWENTYSEVENHUNDRED) {
+			weightClass = ONE;
+            fee = ONE_FEE;
+		} else if (weight <= THIRTYEIGHTHUNDRED) {
+			weightClass = TWO;
+            fee = TWO_FEE;
+		} else if (weight > THIRTYEIGHTHUNDRED) {
+			weightClass = THREE;
+            fee = THREE_FEE;
+		}
+	} else if (year <= 1979) {
+		if (weight < TWENTYSEVENHUNDRED) {
+			weightClass = FOUR;
+            fee = FOUR_FEE;
+		} else if (weight <= THIRTYEIGHTHUNDRED) {
+			weightClass = FIVE;
+            fee = FIVE_FEE;
+		} else if (weight > THIRTYEIGHTHUNDRED) {
+			weightClass = SIX;
+            fee = SIX_FEE;
+		}
+	} else if (year >= 1980) {
+		if (weight < THIRTYFIVEHUNDRED) {
+			weightClass = SEVEN;
+            fee = SEVEN_FEE;
+		} else if (weight >= THIRTYFIVEHUNDRED) {
+			weightClass = EIGHT;
+            fee = EIGHT_FEE;
+		}
+	}
+}
+
+void reportFee() {}
